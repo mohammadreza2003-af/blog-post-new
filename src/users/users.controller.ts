81a959f4798005cad17e7cd49d8c67a4470e7750
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-11-27 11:26:02 +0430

    feat: enhance getUsers endpoint to support pagination with limit and page query parameters

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,26 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Delete,
   Get,
   Param,
   ParseIntPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('{/:id}')
-  public getUsers(@Param('id', ParseIntPipe) id: number | undefined) {
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log('ID:', id);
+    console.log('limit and page:', limit, page);
     return 'This is Users Controller';
   }
   @Post('/')
