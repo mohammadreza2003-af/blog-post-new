bda4c28374a0662a1e3f41dd3519559398d0ce0c
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-12-06 20:04:59 +0430

    refactor: add validation to create post dto

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,5 +1,17 @@
-import { IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+} from 'class-validator';
 import { postStatus, postType } from '../enums';
+import { Optional } from '@nestjs/common';
 
 export class CreatePostDto {
   @IsString()
@@ -8,15 +20,40 @@ export class CreatePostDto {
   title: string;
 
   @IsEnum(postType)
+  @IsNotEmpty()
   postType: postType;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches('/[a-zA-Z-_#.+~/&?=:%0-9]+/')
   slug: string;
 
   @IsEnum(postStatus)
+  @IsNotEmpty()
+  @IsString()
   status: postStatus;
+
+  @IsString()
+  @Optional()
   content?: string;
+
+  @IsOptional()
+  @IsJSON()
   schema?: string;
+
+  @IsOptional()
+  @IsUrl()
   featuredImageUrl?: string;
+
+  @IsISO8601()
+  @IsOptional()
   publishOn: Date;
+
+  @Optional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
   tags: string[];
+
   metaOptions: { key: string; value: string }[];
 }
